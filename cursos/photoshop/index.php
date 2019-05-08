2c6341a225c8dd77833326c52407aa4f4bcb3b44
carlosEH8192 <carloseh.355@gmail.com>
2019-05-08 00:44:49 -0300

    Ajuste de sintaxe nos arquivos do curso 'photoshop': 'index.php' e 'index.php'.

--- cursos/photoshop/index.php
+++ cursos/photoshop/index.php
@@ -1,4 +1,4 @@
-<?php // => /cursos/photoshop/index.php  
+<?php
     set_include_path($_SERVER["DOCUMENT_ROOT"]."/ProjetoFinalGit/");
     include_once("defaultPaths/paths.php");
     include_once("./index.html");
