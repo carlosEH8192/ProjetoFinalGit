4e3d271b927290578eff7b168de01ef85fa12258
carlosEH8192 <carloseh.355@gmail.com>
2019-05-08 01:19:34 -0300

    Pasta 'defaultPaths' excluída. Criado arquivo 'default_paths.php' na raíz do projeto. Referências para o mesmo corrigidas.

--- cadastro/index.php
+++ cadastro/index.php
@@ -1,5 +1,5 @@
 <?php // => /cadastro/index.php
     set_include_path($_SERVER["DOCUMENT_ROOT"]."/ProjetoFinalGit/");
-    include_once("defaultPaths/paths.php");
+    include_once("default_paths.php");
     include_once("./index.html");
 ?>
--- login/index.php
+++ login/index.php
@@ -41,7 +41,7 @@
         session_destroy();
 
         set_include_path($_SERVER["DOCUMENT_ROOT"]."/ProjetoFinalGit/");
-        include_once("defaultPaths/paths.php");
+        include_once("default_paths.php");
         include_once("./index.html");
     }
 ?>
