f4592c734be36ef28c6ed855c352bfcdac800e07
carlosEH8192 <carloseh.355@gmail.com>
2019-05-13 19:22:17 -0300

    Ajuste de sintaxe e remoção de espaços em branco.

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
     $parametro_login_existe = isset($_GET["login"]);
 
     if($parametro_login_existe && $_GET["login"] == "adm") {
-        session_id("admLogin"); // Id da Session configurada para ser Adm por padrão.        
+        session_id("admLogin"); // Id da Session configurada para ser Adm por padrão.
         
         if ($_GET["login"] == "prof")
             session_id("profLogin");
@@ -12,7 +12,7 @@
         if ($_GET["login"] == "adm") {
             $_SESSION["descricao"] = "admLogin";
             session_write_close();
-            header("Location: form_adm.php");
+            header("Location: form_adm.php");        
         } else if ($_GET["login"] == "prof") {
             $_SESSION["descricao"] = "profLogin";
             session_write_close();
@@ -20,9 +20,10 @@
         }
 
         unset($_GET);
-    } else if (session_status() == PHP_SESSION_ACTIVE) {
+    }    
+    else if (session_status() == PHP_SESSION_ACTIVE)
         session_destroy();
-    } else {
+    else {
         include_once("../default_paths.php");
         include_once("index.html");
     }
