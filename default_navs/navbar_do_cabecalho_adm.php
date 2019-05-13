8bccd7b7e1d8c2448f3299d090d208e6663f43b8
carlosEH8192 <carloseh.355@gmail.com>
2019-05-13 20:30:27 -0300

    Logout: ajustado 'include_once' para o arquivo 'default_paths.php'; 'navbar_do_cabecalho_adm.php': ajustados caminhos dos arquivos de gestão (Administradores, Alunos, Cursos, etc.).

--- default_navs/navbar_do_cabecalho_adm.php
+++ default_navs/navbar_do_cabecalho_adm.php
@@ -2,12 +2,13 @@
     include_once("../default_paths.php");
 
     $username = $_SESSION["username"];
-    $gestao_adms = $pasta_raiz . "/adm/adms/index.php";
-    $gestao_alunos = $pasta_raiz . "/adm/alunos/index.php";
-    $gestao_cursos = $pasta_raiz . "/adm/cursos/index.php";
-    $gestao_nomes_turma = $pasta_raiz . "/adm/nomesTurma/index.php";
-    $gestao_professores = $pasta_raiz . "/adm/professores/index.php";
-    $gestao_turmas = $pasta_raiz . "/adm/turmas/index.php";
+    $adm = $pasta_raiz . "adm/";
+    $gestao_adms = $adm . "adms/index.php";
+    $gestao_alunos = $adm . "alunos/index.php";
+    $gestao_cursos = $adm . "cursos/index.php";
+    $gestao_nomes_turma = $adm . "nomesTurma/index.php";
+    $gestao_professores = $adm . "professores/index.php";
+    $gestao_turmas = $adm . "turmas/index.php";
 
     include_once("navbar_do_cabecalho_adm.html");
 ?>
