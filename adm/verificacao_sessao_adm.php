fb180cab9929379d904101d1bf235b8ed30b186a
carlosEH8192 <carloseh.355@gmail.com>
2019-05-13 19:55:23 -0300

    Criada classe 'verificacao_adm' no arquivo 'verificacao_sessao_adm.php'. Métodos adicionados: sessao() e mostra_pagina().

--- adm/verificacao_sessao_adm.php
+++ adm/verificacao_sessao_adm.php
@@ -1,13 +1,31 @@
 <?php
-    function verifica_sessao_adm() {
+public class verificacao_adm {
+    private string $caminho_do_arquivo;
+
+    function __construct($caminho_do_arquivo) {
+        $this->caminho_do_arquivo = $caminho_do_arquivo;
+        include_once("../default_paths.php");
+    }
+
+    private function sessao() {
         session_id("adm");
         session_start();
 
         if(isset($_SESSION["descricao"]) && $_SESSION["descricao"] == "adm")
-            return true;        
+            return true;
         else {
             session_destroy();
             return false;
         }
     }
+
+    public function mostra_pagina() {
+        if (!$this->sessao())
+            return false;
+
+        $navbar_do_cabecalho_adm = $default_navbars . "navbar_do_cabecalho_adm.php";
+        include_once($this->caminho_do_arquivo);
+        return true;
+    }
+}
 ?>
