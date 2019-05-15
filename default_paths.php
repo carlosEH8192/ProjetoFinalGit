5a0fd30975ee54e756dfe407b911dda1fdcbd4fb
carlosEH8192 <carloseh.355@gmail.com>
2019-05-15 02:16:41 -0300

    Forma antiga de guardar caminhos para arquivos usados em todo o Website deletado.

--- default_paths.php
+++ default_paths.php
@@ -70,34 +70,4 @@ class default_paths {
     public function get_logout() { return $this->logout; }
     //endregion
 }
-
-    //region Modo antigo de manter controle dos caminhos do Website
-    $pasta_raiz = "/ProjetoFinalGit/";
-    $caminho_completo_pasta_raiz = $_SERVER["DOCUMENT_ROOT"] . "/ProjetoFinalGit/";
-
-    // Caminhos auxiliares;
-    $bootstrap = $pasta_raiz . "bootstrap/";
-    $bootstrap_css = $bootstrap . "css/bootstrap.min.css";
-    $bootstrap_js = $bootstrap . "js/bootstrap.min.js";
-
-    $img = $pasta_raiz . "img/";
-    $adjust = $img . "adjust.css";
-    $logo_cabecalho = $img . "logo/cb-am.png";
-    $logo_adjust = $img . "logo/logo-adjust.css";
-    $background = $img . "background.png";
-
-    $default_css = $pasta_raiz . "default_css/";
-    $overflow = $default_css . "overflow_y.css";
-    $form = $default_css . "form.css";
-
-    $default_navbars = $caminho_completo_pasta_raiz . "default_navs/";
-    $navbar_do_cabecalho = $default_navbars . "navbar_do_cabecalho.php";
-    $navbar_do_rodape = $default_navbars . "navbar_do_rodape.php";
-
-    $jquery = $pasta_raiz . "jquery.min.js";
-    $colors = $pasta_raiz . "colors/colors.css";
-    $icones = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-
-    $logout = $pasta_raiz . "logout/index.php";
-    //endregion
 ?>
