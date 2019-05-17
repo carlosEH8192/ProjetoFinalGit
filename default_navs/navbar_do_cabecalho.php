dced58f0aeff9f61c401a847e27cb9bedcb33d59
carlosEH8192 <carloseh.355@gmail.com>
2019-05-17 20:04:32 -0300

    Arquivos 'navbar_do_cabecalho.php' e 'navbar_do_cabecalho.html' ajustados para renderizarem a parte de Login corretamente; Arquivos da pasta 'navbar_de_login_sobreposicao' movidos para 'default_navs' e Arquivos .html ajustados para renderizarem corretamente com os caminhos necessários de redirecionamento; Arquivos 'navbar_do_cabecalho_prof' movidos para a pasta 'professor'; Arquivos 'navbar_do_cabecalho_adm' movidos para a pasta 'adm'.

--- default_navs/navbar_do_cabecalho.php
+++ default_navs/navbar_do_cabecalho.php
@@ -1,1 +1,7 @@
-<?php include_once("navbar_do_cabecalho.html"); ?>
+<?php
+    if (session_status() == PHP_SESSION_NONE)
+        session_start();
+
+    $descricao_existe_e_eh_aluno = isset($_SESSION["descricao"]) && $_SESSION["descricao"] == "aluno";
+    include_once("navbar_do_cabecalho.html");
+?>
