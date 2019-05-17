5dea59885563d6626dd4abaae0eb50758e96d720
carlosEH8192 <carloseh.355@gmail.com>
2019-05-17 20:36:40 -0300

    Classe 'redirecionador' ('redirecionamento.php') criada para verificar se existe alguma sessão ativa e, se houver, o usuário é redirecionado para a Página Inicial do Website; Formulário de Login para Aluno, Administrador e Professor ajustados para serem acessíveis sem que seja necessária passar um parâmetro via URL do navegador).

---
+++ login/adm.php
@@ -0,0 +1,9 @@
+<?php
+    include_once("../default_paths.php");
+    include_once("redirecionamento.php");
+
+    $paths = new default_paths();
+    $redirecionador = new redirecionador();
+    $redirecionador->redireciona_para_pagina_inicial_se_alguma_sessao_existir($paths->get_inicio());
+    include_once("adm.html");
+?>
