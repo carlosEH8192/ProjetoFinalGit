fb63be0a62c47161d70b9efeafb9fbbcc1917a4a
carlosEH8192 <carloseh.355@gmail.com>
2019-05-17 22:57:37 -0300

    Classe e Arquivo deletados.

--- adm/obtentor_de_caminhos.php
+++
@@ -1,7 +0,0 @@
-<?php 
-class obtentor_de_caminhos {
-    public function navbar_do_cabecalho_adm() {
-        return (new default_paths())->get_default_navbars() . "navbar_do_cabecalho_adm.php";
-    }
-}
-?>
