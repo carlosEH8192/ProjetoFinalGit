afe0a610c92f4777ee3b47f86a6d8b319d51b9a8
carlosEH8192 <carloseh.355@gmail.com>
2019-05-20 16:37:54 -0300

    Correção de sintaxe e '//region's.

--- default_paths.php
+++ default_paths.php
@@ -1,6 +1,6 @@
 <?php
 class default_paths {
-    //region Props iniciais
+    //region Props Iniciais
     private $pasta_raiz;
     private $caminho_completo_pasta_raiz;
 
@@ -9,7 +9,7 @@ class default_paths {
     private $contato;
     private $cadastro;
     //endregion
-    
+
     //region Props Cursos
     private $cursos;
     private $photoshop;
@@ -17,12 +17,13 @@ class default_paths {
     private $ds_max;
     private $informatica_basica;
     //endregion
-    //region Props Bootstrap
+
+    // Props Bootstrap
     private $bootstrap;
     private $bootstrap_css;
     private $bootstrap_js;
-    //endregion
-    //region Img e Logo
+
+    //region Props Img e Logo
     private $img;
     private $adjust;
     private $background;
@@ -30,22 +31,22 @@ class default_paths {
     private $logo_cabecalho;
     private $logo_adjust;
     //endregion
-    //region Default CSS
+
+    // Props Default CSS
     private $default_css;
     private $overflow;
     private $form;
-    //endregion
-    //region Default Navabars
+
+    // Props Default Navabars
     private $default_navbars;
     private $navbar_do_cabecalho;
     private $navbar_do_rodape;
-    //endregion
 
-    //region Props Finais
+    // Props Finais
     private $jquery;
     private $colors;
     private $icones;
-    //endregion
+
     //region Props Login e Logout
     private $login_aluno;
     private $login_adm;
@@ -54,7 +55,7 @@ class default_paths {
     //endregion
 
     public function __construct() {
-        //region Iniciais
+        //region Props Iniciais
         $this->pasta_raiz = "/ProjetoFinalGit/";
         $this->caminho_completo_pasta_raiz = $_SERVER["DOCUMENT_ROOT"] . $this->pasta_raiz;
         
@@ -64,19 +65,20 @@ class default_paths {
         $this->cadastro = $this->pasta_raiz . "cadastro/index.php";
         //endregion
 
-        //region Cursos
+        //region Props Cursos
         $this->cursos = $this->pasta_raiz . "cursos/";
         $this->photoshop = $this->cursos . "photoshop/index.php";
         $this->corel_draw = $this->cursos . "corel_draw/index.php";
         $this->ds_max = $this->cursos . "3ds_max/index.php";
         $this->informatica_basica = $this->cursos . "informatica_basica/index.php";
         //endregion
-        //region Bootstrap
+
+        // Props Bootstrap
         $this->bootstrap = $this->pasta_raiz . "bootstrap/";
         $this->bootstrap_css = $this->bootstrap . "css/bootstrap.min.css";
         $this->bootstrap_js = $this->bootstrap . "js/bootstrap.min.js";
-        //endregion
-        //region Img e Logo
+
+        //region Props Img e Logo
         $this->img = $this->pasta_raiz . "img/";
         $this->adjust = $this->img . "adjust.css";
         $this->background = $this->img . "background.png";
@@ -84,23 +86,23 @@ class default_paths {
         $this->logo_cabecalho = $this->logo . "cb-am.png";
         $this->logo_adjust = $this->logo . "logo-adjust.css";
         //endregion
-        //region Default CSS
+
+        // Props Default CSS
         $this->default_css = $this->pasta_raiz . "default_css/";
         $this->overflow = $this->default_css . "overflow_y.css";
         $this->form = $this->default_css . "form.css";
-        //endregion
-        //region Default Navbars
+
+        // Props Default Navbars
         $this->default_navbars = $this->caminho_completo_pasta_raiz . "default_navs/";
         $this->navbar_do_cabecalho = $this->default_navbars . "navbar_do_cabecalho.php";
         $this->navbar_do_rodape = $this->default_navbars . "navbar_do_rodape.php";
-        //endregion
 
-        //region Finais
+        // Props Finais
         $this->jquery = $this->pasta_raiz . "jquery.min.js";
         $this->colors = $this->pasta_raiz . "colors/colors.css";
         $this->icones = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
-        //endregion
-        //region Login
+
+        //region Props Login e Logout
         $this->login_aluno = $this->pasta_raiz . "login/aluno.php";
         $this->login_adm = $this->pasta_raiz . "login/adm.php";
         $this->login_professor = $this->pasta_raiz . "login/professor.php";
@@ -122,31 +124,32 @@ class default_paths {
     public function get_ds_max() { return $this->ds_max; }
     public function get_informatica_basica() { return $this->informatica_basica; }
     //endregion
-    //region Getters Bootstrap
+
+    // Getters Bootstrap
     public function get_bootstrap_css() { return $this->bootstrap_css; }
     public function get_bootstrap_js() { return $this->bootstrap_js; }
-    //endregion
+
     //region Getters Img e Logo
     public function get_adjust() { return $this->adjust; }
     public function get_background() { return $this->background; }
     public function get_logo_cabecalho() { return $this->logo_cabecalho; }
     public function get_logo_adjust() { return $this->logo_adjust; }
     //endregion
-    //region Getters Default CSS
+
+    // Getters Default CSS
     public function get_overflow() { return $this->overflow; }
     public function get_form() { return $this->form; }
-    //endregion
-    //region Getters Default Navbars
+
+    // Getters Default Navbars
     public function get_default_navbars() { return $this->default_navbars; }
     public function get_navbar_do_cabecalho() { return $this->navbar_do_cabecalho; }
     public function get_navbar_do_rodape() { return $this->navbar_do_rodape; }
-    //endregion
 
-    //region Getters Finais
+    // Getters Finais
     public function get_jquery() { return $this->jquery; }
     public function get_colors() { return $this->colors; }
     public function get_icones() { return $this->icones; }
-    //endregion
+
     //region Getters Login e Logout
     public function get_login_aluno() { return $this->login_aluno; }
     public function get_login_adm() { return $this->login_adm; }
