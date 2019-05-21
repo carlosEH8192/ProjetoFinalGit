f3fd722c2fd61e609907ea188c5893ab7b6628c4
carlosEH8192 <carloseh.355@gmail.com>
2019-05-20 23:27:12 -0300

    Adicionada propriedade 'codigo' na Model 'adm'.

--- model/adm.php
+++ model/adm.php
@@ -1,14 +1,19 @@
 <?php 
-class adm {
+class adm implements JsonSerializable {
+    private $codigo;
     private $username;
     private $senha;
 
-    public function __construct($username, $senha) {
+    public function __construct($codigo, $username, $senha) {
+    	$this->codigo = $codigo;
         $this->username = $username;
         $this->senha = $senha;
     }
 
+    public function get_codigo() { return $this->codigo; }
     public function get_username() { return $this->username; }
     public function get_senha() { return $this->senha; }
+
+    public function jsonSerialize() { return get_object_vars($this); }
 }
 ?>
