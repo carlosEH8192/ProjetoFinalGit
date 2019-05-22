aef3d41edf79a99554bdca1bd84136eae9946a48
carlosEH8192 <carloseh.355@gmail.com>
2019-05-22 01:42:43 -0300

    Criada Classe 'Crud' para ser utilizada em todas as páginas de Consulta/Edição/Remoção/Inserção de dados no banco de dados; Arquivos atualiza_adm.html e insere_adm.html corrigidos para fazerem a inserção devidamente; Novo arquivo: index.js - contém a utilização da Classe Crud que é utilizado na página index.html.

---
+++ adm/Crud.js
@@ -0,0 +1,40 @@
+class Crud {
+    constructor(uriBusca, uriDeleta, formatoTr, $tbody, $inputFiltra) {
+        this.uriBusca = uriBusca;
+        this.uriDeleta = uriDeleta;
+        this.formatoTr = formatoTr; // Formato da Table Row
+        this.$tbody = $tbody; // Elemento tbody dos registros filtrados;
+        this.$inputFiltra = $inputFiltra; // Elemento Input de Filtragem;
+    }
+
+    filtra(evento) {
+        if (evento.which == 13)
+            this.busca();
+    }
+
+    busca() {
+        var filtro = (this.$inputFiltra.val() != null) ? this.$inputFiltra.val() : null;
+        console.log()
+
+        $.get(this.uriBusca, { filtro: filtro }, (jsonResposta) => {
+            this.montaTabela(JSON.parse(jsonResposta));
+        });
+    }
+
+    limpaTabela() { this.$tbody.empty(); }
+
+    montaTabela(jsonResposta) {
+        this.limpaTabela();
+        var trs = this.geraTrs(jsonResposta);
+        this.$tbody.html(trs);
+    }
+
+    // Função deve ser implementada
+    geraTrs(jsonResposta) { throw 'Função "geraTrs(jsonResposta)" deve ser implementada antes de ser usada.' }
+
+    deleta(codigo) {
+        $.post(this.uriDeleta, { codigo: codigo }, () => {
+            this.busca();
+        });
+    }
+}
---
+++ adm/adms/index.js
@@ -0,0 +1,21 @@
+var formatoTr =
+    '<tr>' +
+        '<th scope="row">@cod</th>' +
+        '<td>@username</td>' +
+        '<td><a href="atualiza_adm.php?codigo=@cod" class="btn btn-default">Editar</a></td>' +
+        '<td><button class="btn btn-danger" onclick="crud.deleta(@cod)">Excluir</td>' +
+    '</tr>';
+
+Crud.prototype.geraTrs = function(admsJson) {
+    var tr = null;
+    for (var i = 0; i < admsJson.length; i++) {
+        var adm = admsJson[i];
+        tr += formatoTr.replace(/@cod/g, adm.codigo)
+            .replace('@username', adm.username);
+    }
+
+    return tr;
+};
+
+var crud = new Crud('_busca_adms.php', '_deleta_adm.php', formatoTr, $('#tabela tbody'), $('#filtro'));
+crud.busca();
